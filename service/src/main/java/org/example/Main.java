e55622d9425e40153bd2dc8d35f6e67dd308d212
Mironov Evgeny <e.d.mironov@yandex.ru>
2026-02-10 23:31:13 +0300

    Initial commit

---
+++ service/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.example.dto.MyEntity;
+import org.example.service.MyService;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyService myService = new MyService();
+        System.out.println(myService.getMyEntity());
+        System.out.println(myService.setMyEntity(new MyEntity("second")));
+        System.out.println(myService.getMyEntity());
+    }
+}
