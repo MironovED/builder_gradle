7ec25a83ff0e0c205c6734bd445f5d2162f541cf
Mironov Evgeny <e.d.mironov@yandex.ru>
2026-02-12 15:54:10 +0300

    исправления по замечаниям ДЗ

--- api/src/main/java/org/example/Main.java
+++ api/src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.example.dto.MyEntity;
+import org.example.service.MyService;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyService myService = new MyService();
+        System.out.println(myService.getMyEntity());
+        System.out.println(myService.setMyEntity(new MyEntity("second")));
+        System.out.println(myService.getMyEntity());
     }
 }
